5f331c4d07f81015a18c9de376518591382ade53
8810yuzuki <8810.yuzuki@gmail.com>
2026-06-19 00:24:25 +0900

    update

--- public/data.js
+++ public/data.js
@@ -10,47 +10,102 @@ const users = [
         name: "りチャ",
         birthday: "09-29",
         hobby: "佐間 龍斗",
-        discordId: ""
+        discordId: "1350650282429907046"
     },
     {
         name: "ちょぱおう",
         birthday: "12-31",
         hobby: "天使さんごっこ",
-        discordId: ""
+        discordId: "1200959211698401330"
     },
     {
         name: "りう",
         birthday: "03-13",
         hobby: "嵐",
-        discordId: ""
+        discordId: "1281107106585509918"
     },
     {
         name: "いのがに",
         birthday: "09-17",
         hobby: "ギター",
-        discordId: ""
+        discordId: "972433783070523413"
     },
     {
         name: "はるゆき",
         birthday: "02-18",
         hobby: "コスプレ",
         image: "image/haruyuki.jpg",
-        discordId: ""
+        discordId: "573135231478136832"
     },
     {
         name: "もねちやん",
         birthday: "12-21",
         hobby: "朝の光の中で",
         image: "image/mone.jpg",
-        discordId: ""
+        discordId: "1225133625549394043"
     },
     {
         name: "るにやーん",
         birthday: "07-18",
         hobby: "冷笑",
         image: "image/runya.jpg",
-        discordId: ""
+        discordId: "1442871972685025341"
     }
+{
+        name: "りお",
+        birthday: "08-22",
+        hobby: "不明",
+        image: "image/rio.jpg",
+        discordId: "1028897226665230417"
+    },
+{
+        name: "アル中",
+        birthday: "不明」",
+        hobby: "残業",
+        image: "image/al.jpg",
+        discordId: "1233993634110636045"
+    },
+{
+        name: "めんちゅ",
+        birthday: "09-18",
+        hobby: "ディズニー",
+        image: "image/men.jpg",
+        discordId: "1218582551883419769"
+    },
+{
+        name: "つばめくん",
+        birthday: "不明",
+        hobby: "不明",
+        image: "image/tsuba.jpg",
+        discordId: "1269349290417848423"
+    },
+{
+        name: "ニック",
+        birthday: "不明",
+        hobby: "不明",
+        image: "image/nick.jpg",
+        discordId: "1032309489933746226"
+    },
+{
+        name: "ぎんちゃん",
+        birthday: "07-18",
+        hobby: "不明",
+        image: "image/gin.jpg",
+        discordId: "690165559156932608"
+    },
+{
+        name: "ねむ",
+        birthday: "不明",
+        hobby: "不明",
+        image: "image/nemu.jpg",
+        discordId: "690165559156932608"
+    },
+
+
+
+
+
+
 ];
 
 
